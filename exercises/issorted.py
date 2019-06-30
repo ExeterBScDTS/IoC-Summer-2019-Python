684ca5ae20a4466fdcc8c9e864207d0313d363e8
Saunby <M.Saunby@exeter.ac.uk>
2019-06-30 19:08:47 +0100

    .

---
+++ exercises/issorted.py
@@ -0,0 +1,18 @@
+# Check if a list is already sorted
+
+data = ["Ant", "Bat", "cat", "elephant", "squirrel", "Dog", "hawk", "spider"]
+
+
+def issorted(items):
+    result = False
+    # Add your code here
+    return result
+
+
+if __name__ == "__main__":
+
+    if issorted(data):
+        print("The list is sorted")
+    else:
+        print("The list is not sorted")
+
